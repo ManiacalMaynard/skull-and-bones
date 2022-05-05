4e8872ee9d0e081f8206f79d5fb1fdfff95f03fb
ManiacalMaynard <maynard@maniacalindustries.com>
2022-05-05 14:44:41 -0700

    minor tweaks, preparing for factory refactor

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 console.log('Behold mortals, a donut!') // habit at this point, just shows me the script loaded, safely ignore =P
 
-// Create an array with 9 empty strings representing the gameBoard square's initial values (X or O symbols when assigned fire win state function to evaluate)
-let gameState = [ '', '', '', '', '', '', '', '', '' ];
-
 // Array of all possible win states indexes for winState() comparison. 
 const gameWinStates = [
   [0, 1, 2],
@@ -18,18 +15,15 @@ const gameWinStates = [
 // build an intro screen with player symbol choice to start the game, this may eventually include much more (see README.md). Also serves as a reset point for playAgain()
 
 // Function to grab the player symbol selection value from start screen and set it to a global variable
-let playerSymbol = '' // 'X' or 'O'
-let computerSymbol = '' // 'X' or 'O'
-function playerSymbolSet() {
-  // playerSymbol = document.getElementByID('playerChoice').value
-  // computerSymbol = playerSymbol == 'X' ? 'O' : 'X';
-  // set intro screen to hidden
-  whosFirst()
-  buildGameBoard()
-};
+function createPlayer(name, symbol) {
+  return {
+    name,
+    symbol,
+  }
+}
+const playerOne = createPlayer(name, symbol)
 
 // Function to decide randomly who goes first: player or computer. Sets global variable "whosTurn" for use with winStat(). Also displays who's turn on the game board with some className
-let playerTurn = '';
 function whosFirst() {
   let who = Math.floor(Math.random() * 2);
   who === 0 ? playerTurn = 0 : playerTurn = 1 ; // this might be better firing either playerPlay() or computerPlay() instead
@@ -37,6 +31,7 @@ function whosFirst() {
 
 // Builder function that populates the gameBoard div with 9 square divs wihtin, each with 0-8 as IDs and filled with the strings from the original array as it changes
 function buildGameBoard() {
+  let gameState = [ '', 'x', '', 'o', '', '', '', '', '' ]; //new Array(9).fill(null)
   // forEach item in gameState build a div with index for ID, easy!
 };
 
@@ -45,7 +40,7 @@ function playerPlay() {
   // animation for "your turn!"
   // something this.innerText &&
   winState()
-  computerTurn()
+  computerPlay()
 };
 
 // Computer logic function to choose a random open square after a short delay. May also need to turn off playerPlay functionality temporarily
